7a172f15a22ece8025e5f2fff50b0dff19856520
em3rc0dTh <eduardo.farid@thradex.com>
2026-08-31 15:04:40 -0500

    feat(mitos-email): wire booking transactional email events

--- backend/src/routes/bookingRoutes.ts
+++ backend/src/routes/bookingRoutes.ts
@@ -3,6 +3,12 @@ import multer from 'multer'
 import routeNames from '../config/bookingRoutes.config'
 import authJwt from '../middlewares/authJwt'
 import { reuseCheckoutSession } from '../middlewares/idempotentCheckout'
+import {
+  cancellationRequestedEmail,
+  reservationCancelledOnBulkUpdate,
+  reservationCancelledOnUpdate,
+  reservationReceivedEmail,
+} from '../middlewares/transactionalEmailEvents'
 import * as bookingController from '../controllers/bookingController'
 import * as rentalLifecycleController from '../controllers/rentalLifecycleController'
 import * as adminBookingSupplierController from '../controllers/adminBookingSupplierController'
@@ -10,9 +16,21 @@ import * as adminBookingSupplierController from '../controllers/adminBookingSupp
 const routes = express.Router()
 
 routes.route(routeNames.create).post(authJwt.verifyToken, bookingController.create)
-routes.route(routeNames.checkout).post(reuseCheckoutSession, bookingController.checkout)
-routes.route(routeNames.update).put(authJwt.verifyToken, bookingController.update)
-routes.route(routeNames.updateStatus).post(authJwt.verifyToken, bookingController.updateStatus)
+routes.route(routeNames.checkout).post(
+  reservationReceivedEmail,
+  reuseCheckoutSession,
+  bookingController.checkout,
+)
+routes.route(routeNames.update).put(
+  authJwt.verifyToken,
+  reservationCancelledOnUpdate,
+  bookingController.update,
+)
+routes.route(routeNames.updateStatus).post(
+  authJwt.verifyToken,
+  reservationCancelledOnBulkUpdate,
+  bookingController.updateStatus,
+)
 routes.route(routeNames.delete).post(authJwt.verifyToken, bookingController.deleteBookings)
 routes.route(routeNames.deleteTempBooking).delete(bookingController.deleteTempBooking)
 routes.route(routeNames.getBooking).get(bookingController.getBooking)
@@ -20,7 +38,11 @@ routes.route(routeNames.getBookingId).get(bookingController.getBookingId)
 routes.route(routeNames.getBookings).post(authJwt.verifyToken, bookingController.getBookings)
 routes.route(routeNames.getAdminBookingSuppliers).get(authJwt.verifyToken, adminBookingSupplierController.getAdminBookingSuppliers)
 routes.route(routeNames.hasBookings).get(authJwt.verifyToken, bookingController.hasBookings)
-routes.route(routeNames.cancelBooking).post(authJwt.verifyToken, bookingController.cancelBooking)
+routes.route(routeNames.cancelBooking).post(
+  authJwt.verifyToken,
+  cancellationRequestedEmail,
+  bookingController.cancelBooking,
+)
 routes.route(routeNames.purchaseOrder).get(bookingController.downloadPurchaseOrder)
 routes.route(routeNames.checkoutReportPdf).get(bookingController.downloadCheckoutReport)
 routes.route(routeNames.checkoutDeparture).post(
