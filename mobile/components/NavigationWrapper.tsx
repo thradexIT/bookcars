71490434343af7362137092fa9cd12dbc3c343f5
em3rc0dTh <eduardo.farid@thradex.com>
2026-09-21 16:36:11 -0500

    feat(mobile): apply Mitos rebrand to components/NavigationWrapper.tsx

--- mobile/components/NavigationWrapper.tsx
+++ mobile/components/NavigationWrapper.tsx
@@ -4,6 +4,7 @@ import { NavigationContainer, NavigationContainerRef } from '@react-navigation/n
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar'
 import Toast from 'react-native-toast-message'
 import DrawerNavigator from '@/components/DrawerNavigator'
+import { mitosColors } from '@/config/mitosBrand'
 
 interface NavigationWrapperProps {
   ref?: React.RefObject<NavigationContainerRef<StackParams> | null>
@@ -19,7 +20,7 @@ const NavigationWrapper: React.FC<NavigationWrapperProps> = ({ onReady, ref: nav
       onReady={onReady}
       onStateChange={refresh}
     >
-      <ExpoStatusBar style="dark" backgroundColor="#ffffff" translucent={false} />
+      <ExpoStatusBar style="light" backgroundColor={mitosColors.navy} translucent={false} />
       <DrawerNavigator key={language} />
       <Toast />
     </NavigationContainer>
